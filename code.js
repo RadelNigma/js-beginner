535fbc487c0cee78bf3d59fc3db05f150507c332
RR.Nigmatullin <radelnigma@yandex.ru>
2022-08-10 20:45:27 +0300

    rabota 10.08.22

---
+++ code.js
@@ -0,0 +1,10 @@
+const url = 'http://localhost:8080/api/users'
+const conrenedor = document.querySelector('tbody')
+let result =''
+
+const modalArticles = new bootstrap.Modal(document.getElementById('modalArticulates'))
+const formArticles = document.querySelector('form')
+const description = document.getElementById('description')
+const presto = document.getElementById('presto')
+const stock = document.getElementById('stock')
+let opcion = ''
